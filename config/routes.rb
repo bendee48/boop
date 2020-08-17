4cba83d24e798d9be63244a247476c51990891f8
Ben Daniels <benbanj@hotmail.com>
2020-08-17 23:26:31 +0100

    Adding User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
+  get 'signup', to: 'users#create'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
