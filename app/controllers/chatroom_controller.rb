008c36223a4302f2557c4b0caf0f539121a32601
Ben Daniels <benbanj@hotmail.com>
2020-08-17 16:57:23 +0100

    restricting message loads

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,7 +2,7 @@ class ChatroomController < ApplicationController
   before_action :require_login
 
   def index
-    @messages = Message.all
+    @messages = Message.latest_messages
     @message = Message.new
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
-
   validates :body, presence: true
+  scope :latest_messages, -> { order(:created_at).last(20) }
 end
